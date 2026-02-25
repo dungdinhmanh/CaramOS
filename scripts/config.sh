db6abae0d171d6a2975fc25a2091c3b1c6aa7153
MINHMC2077 <106906918+minhmc2007@users.noreply.github.com>
2026-02-25 10:39:25 +0700

    chore: Update Mint version + mirror (#2)

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 # Cấu hình build — đổi version/mirror ở đây
 
-MINT_VERSION="22"
+MINT_VERSION="22.3"
 MINT_EDITION="cinnamon"
 MINT_ARCH="64bit"
 MINT_ISO_NAME="linuxmint-${MINT_VERSION}-${MINT_EDITION}-${MINT_ARCH}.iso"
-MINT_MIRROR="https://mirrors.kernel.org/linuxmint/stable/${MINT_VERSION}/${MINT_ISO_NAME}"
-
+MINT_MIRROR="https://mirror.meowsmp.net/linuxmint/iso/stable/${MINT_VERSION}/${MINT_ISO_NAME}"
 CARAMOS_VERSION="0.1"
 OUTPUT_ISO="CaramOS-${CARAMOS_VERSION}-${MINT_EDITION}-amd64.iso"
 WORK_DIR="./build"
-
-# Nén mặc định: lz4 (nhanh, dev). --release sẽ đổi sang xz (nhỏ)
+# Nén mặc định: lz4 (nhanh cho dev). --release sẽ đổi sang xz (nhỏ, nén lâu)
 SQUASHFS_COMP="lz4"
 SQUASHFS_OPTS="-noappend"
